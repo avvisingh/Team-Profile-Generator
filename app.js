ae96580577e5504e1086f21336be0bb0432df83b
unknown <singh.avvi98@gmail.com>
2020-12-14 17:26:33 +1100

    Added basic prompts and beginning to build function which will put all prompter functions together

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
+const chalk = require('chalk');
 const path = require("path");
 const fs = require("fs");
 
@@ -10,9 +11,11 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+let wantsToAddEmployee = true;
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+console.log(chalk.bgYellowBright('Would you like to add an employee?'));
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
